3e4f05299c033a9d01155cdf9855aa34dfddc16c
Lucas Nascimento <lucasnascimentoribeiro13@gmail.com>
2021-04-22 19:52:32 -0300

    Transform values on add

--- src/model/Database.h
+++ src/model/Database.h
@@ -12,12 +12,16 @@ using terms_type = vector<Term*>;
 class Database {
  private:
   terms_type terms;
+  void sort_by_weight(terms_type terms) {
+    sort(terms.begin(), terms.end(), [](Term* a, Term* b) { return a->weight < b->weight; });
+  }
 
  public:
   terms_type get_terms() {
     return this->terms;
   }
   void add_term(Term* term) {
+    transform(term->value.begin(), term->value.end(), term->value.begin(), ::tolower);
     terms.push_back(term);
   }
   terms_type find_terms(string& query) {
@@ -29,14 +33,11 @@ class Database {
 
     for (size_t i = 0; i < terms.size(); i++) {
       term = terms[i];
-
-      term_value = term->value;
-      transform(term_value.begin(), term_value.end(), term_value.begin(), ::tolower);
-
-      if (term_value.find(query) == 0)
+      if (term->value.find(query) == 0)
         matchs.push_back(term);
     }
 
+    sort_by_weight(matchs);
     return matchs;
   }
 };
